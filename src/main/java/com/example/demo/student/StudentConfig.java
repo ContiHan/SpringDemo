b9a2a67acf30f9eb5382eb2155e7791aded42ed7
Daniel Hanák <daniel.hanak@outlook.cz>
2023-03-21 19:11:49 +0100

    added some objects into the DB

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.FEBRUARY;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args ->  {
+            Student johnDoe = new Student(
+                    "John Doe",
+                    "j.doe@gmail.com",
+                    LocalDate.of(2000, FEBRUARY, 20),
+                    23
+            );
+
+            Student joeBlack = new Student(
+                    "Joe Black",
+                    "j.black@gmail.com",
+                    LocalDate.of(2002, FEBRUARY, 15),
+                    21
+            );
+
+            studentRepository.saveAll(
+                    List.of(johnDoe, joeBlack)
+            );
+        };
+    }
+}
