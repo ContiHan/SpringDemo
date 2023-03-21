d7fb46bab90a4299515c3981f4421de248cfe5c7
Daniel Hanák <daniel.hanak@outlook.cz>
2023-03-21 18:53:40 +0100

    added student model into the DB by annotations

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,22 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
@@ -12,7 +26,11 @@ public class Student {
     public Student() {
     }
 
-    public Student(Long id, String name, String email, LocalDate dateOfBirth, Integer age) {
+    public Student(Long id,
+                   String name,
+                   String email,
+                   LocalDate dateOfBirth,
+                   Integer age) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -20,7 +38,10 @@ public class Student {
         this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dateOfBirth, Integer age) {
+    public Student(String name,
+                   String email,
+                   LocalDate dateOfBirth,
+                   Integer age) {
         this.name = name;
         this.email = email;
         this.dateOfBirth = dateOfBirth;
