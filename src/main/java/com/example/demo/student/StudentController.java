d2e122f76b7c0ef102fed2cd4b8443f9efa3caf4
Daniel Hanák <daniel.hanak@outlook.cz>
2023-03-21 15:34:55 +0100

    injected student service

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,23 +1,23 @@
 package com.example.demo.student;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
-    @GetMapping(path = "api/v1/student")
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "John Doe",
-                        "j.doe@gmail.com",
-                        LocalDate.of(2000, Month.FEBRUARY, 20),
-                        23
-                ));
+        return studentService.getStudents();
     }
 }
